c09c553fb43938d823f0e28c747fdc4954a7091d
Nate Coraor <nate@bx.psu.edu>
2017-09-19 10:49:37 -0400

    Test Travis env to figure out permissions problems

---
+++ travis-env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pdl="$(command -v pulsar-drmaa-launch)"
+
+echo "umask is $(umask)"
+echo "pwd is $(pwd)"
+echo "\$HOME is $HOME"
+echo "\$VIRTUAL_ENV is $VIRTUAL_ENV"
+echo "\$DRMAA_LIBRARY_PATH is $DRMAA_LIBRARY_PATH"
+echo "pulsar-drmaa-launch is $pdl"
+
+for path in $pdl $DRMAA_LIBRARY_PATH; do
+    a=(${path//\// })
+    p=''
+    for i in "${!a[@]}"; do
+        p+="/${a[$i]}"
+        ls -ld "$p"
+    done
+done
